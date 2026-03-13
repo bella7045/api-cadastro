6b39b23deb2a95eeeaf76e0540051f37d5751f30
bella7045 <andrade.santos24@escola.pr.gov.br>
2026-03-13 09:59:19 -0300

    feat: casdastro finalizado

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require('express'); //servidor web
+const fs = require('fs'); //manipulação de arquivos
+const path = require('path'); //manipulação de caminhos
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+/*
+CLIENTES ENDEPOINTS
+*/
 const clientesFile = path.join(__dirname, 'clientes.json');
 
-function lerClientes() {
+function lerclientes() {
     if (!fs.existsSync(clientesFile)) {
+        return [];
+
+    }
+    const dados = fs.readFileSync(clientesFile, 'utf-8');
+    try {
+        return JSON.parse(dados) || [];
+    } catch (e) {
         return [];
     }
 
-    const dados = fs.readFileSync(clientesFile, 'utf-8');
-    return JSON.parse(dados || '[]');
 }
 
 function salvarClientes(clientes) {
@@ -23,31 +31,28 @@ function salvarClientes(clientes) {
 }
 
 app.post('/clientes', (req, res) => {
-
     const { cpf, nome, idade, endereco, bairro, contato } = req.body;
 
     if (!cpf || !nome || !idade || !endereco || !bairro || !contato) {
-        return res.status(400).json({ erro: "Todos os campos são obrigatórios" });
+        return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
     }
 
-    const clientes = lerClientes();
+    const clientes = lerclientes();
 
     if (clientes.some(c => c.cpf === cpf)) {
-        return res.status(400).json({ erro: "CPF já cadastrado" });
+        return res.status(400).json({ error: 'CPF já cadastrado' });
     }
 
     const novoCliente = { cpf, nome, idade, endereco, bairro, contato };
-
     clientes.push(novoCliente);
     salvarClientes(clientes);
 
-    res.status(201).json({
-        mensagem: "Cliente cadastrado com sucesso",
-        cliente: novoCliente
-    });
+    res.status(201).json({ message: 'Cliente cadastrado com sucesso', cliente: novoCliente });
+
 
 });
 
+
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost: ${port}`);
 });
